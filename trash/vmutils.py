60e6f7526819dc0134ebf6da269ba93bedcf5a8a
dusty <bsdpunk@gmail.com>
2016-08-15 13:55:55 -0500

    notquite poweron

--- trash/vmutils.py
+++ trash/vmutils.py
@@ -799,3 +799,33 @@ def esx_destroy_vm(key, si):
     TASK = VM.Destroy_Task()
     wait_for_tasks(si, [TASK])
     print("Done.")
+
+def esx_poweron(key, si):
+
+  try:
+      vmnames = key['vmarg']
+      if not len(vmnames):
+         print("No virtual machine specified for poweron")
+         sys.exit()
+
+
+      # Retreive the list of Virtual Machines from the inventory objects
+      # under the rootFolder
+      content = si.content
+      objView = content.viewManager.CreateContainerView(content.rootFolder,
+                                                        [vim.VirtualMachine],
+                                                        True)
+      vmList = objView.view
+      objView.Destroy()
+
+      # Find the vm and power it on
+      tasks = [vm.PowerOn() for vm in vmList if vm.name in vmnames]
+
+      # Wait for power on to complete
+      WaitForTasks(tasks, si)
+
+      print("Virtual Machine(s) have been powered on successfully")
+  except vmodl.MethodFault as e:
+      print("Caught vmodl fault : " + e.msg)
+  except Exception as e:
+      print("Caught Exception : " + str(e)) 
